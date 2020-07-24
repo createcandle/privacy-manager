2729d85ab596e54c90f1bfcfcb9a3c4ed6084984
Michael Stegeman <michael@stegeman.me>
2020-07-24 12:21:02 -0800

    Clean up packaging and drop package.json.

--- package.sh
+++ package.sh
@@ -1,15 +1,32 @@
-#!/bin/bash
+#!/bin/bash -e
 
-version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
-rm -rf SHA256SUMS package
-rm -rf ._*
+# Clean up from previous releases
+rm -rf *.tgz package SHA256SUMS
+
+# Prep new package
 mkdir package
+
+# Put package together
 cp *.py manifest.json LICENSE README.md package/
 cp -r pkg css images js views package/
-cd package
-find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
-cd ..
+find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
+find package -type d -empty -delete
 
-tar czf "privacy-manager-${version}.tgz" package
-sha256sum "privacy-manager-${version}.tgz"
+# Generate checksums
+echo "generating checksums"
+cd package
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
+echo "creating archive"
+TARFILE="privacy-manager-${version}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+cat ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
